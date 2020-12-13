1c6aaed03590e944a141a24841bb1b5e9aabab96
RohanK22 <rohank2205@gmail.com>
2020-12-13 19:54:44 +0000

    Insertion Sort

---
+++ main.py
@@ -0,0 +1,11 @@
+# Insertion Sort on a List 
+def insertionSort(l):
+  for j in range(1, len(l)): 
+    # Note: range(start, end) does not include the end  
+    key = l[j]
+    i = j - 1
+    while i >= 0 and l[i] > key:
+      l[i + 1] = l[i]
+      i = i - 1
+    l[i+1] = key
+  return l
